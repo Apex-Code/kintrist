9aadf74738edbcddc862708a7ed271c7657db50e
Apex-Code <upstaterounder@gmail.com>
2017-07-26 22:43:34 -0500

    add edit, update, destroy methods to BooksController, add before action for find book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
 
+before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+
   def index
   	@books = Book.all
   end
@@ -12,10 +15,26 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   end
 
+  def show
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  	@book.destroy
+  end
 
 private
 
   def book_params
   	params.require(:book).permit(:title, :description, :author)
+  end
+
+  def find_book
+    @book = Book.find(params[:id])
   end
 end
