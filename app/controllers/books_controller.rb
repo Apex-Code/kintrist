6fb620021576162be09de25032652f05de9bb91d
Apex-Code <upstaterounder@gmail.com>
2017-07-26 22:39:12 -0500

    add create and new method for bookscontroller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,18 @@ class BooksController < ApplicationController
   	@books = Book.all
   end
 
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  end
+
+
+private
+
+  def book_params
+  	params.require(:book).permit(:title, :description, :author)
+  end
 end
