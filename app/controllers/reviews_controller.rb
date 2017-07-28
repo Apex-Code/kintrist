80bbf330440a8dec202a42bbed0360f4c4c67584
Apex-Code <upstaterounder@gmail.com>
2017-07-27 20:10:04 -0500

    add new, update, destroy, show, etc to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+
+	def new
+		@review = Review.new
+	end
+
+	def create
+		@review = Review.new(review_params)
+		@review.book_id = @book.id
+		@review.user_id = current_user.id
+
+		if @review.save
+			redirect_to book_path(@book)
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @review.update(review_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@review.destroy
+		redirect_to book_path(@book)
+	end
+
 end
