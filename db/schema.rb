acc4fd55e3605453b42078bdbca95f3d56aa4796
Apex-Code <upstaterounder@gmail.com>
2017-07-27 19:43:27 -0500

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728003720) do
+ActiveRecord::Schema.define(version: 20170728004208) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20170728003720) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
